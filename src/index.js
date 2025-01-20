5f4877560873b74b2868d4443a0aaa706eec1ef6
znabhi <znabhi02@gmail.com>
2025-01-20 19:46:15 +0530

    Adding roles and user_roles through table

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const { PORT } = require("./config/serverConfig");
 const app = express();
 const apiRoutes = require("./routers/index");
 const bodyParser = require("body-parser");
+const { User, Role } = require("./models/index");
 const parpareServer = () => {
   app.use(bodyParser.json());
 
@@ -10,7 +11,13 @@ const parpareServer = () => {
 
   app.use("/api", apiRoutes);
 
-  app.listen(PORT, () => {
+  app.listen(PORT, async () => {
+    const u1 = await User.findByPk(3);
+    const r1 = await Role.findByPk(1);
+    // u1.addRole(r1);
+    const response = await u1.hasRole(r1);
+    console.log(response);
+
     console.log("Server is stated", PORT);
   });
 };
